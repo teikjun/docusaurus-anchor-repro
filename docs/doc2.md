11276fa7a8e582831bf59b983aaafc657ba5841d
teikjun <teikjunhci@gmail.com>
2020-06-09 01:02:59 +0800

    add link to doc1

--- docs/doc2.md
+++ docs/doc2.md
@@ -7,4 +7,6 @@ title: Document Number 2
 
 - [go to code](doc1#code)
 
+- [go to doc1](doc1)
+
 This is a link to [another document.](doc3.md) This is a link to an [external page.](http://www.example.com/)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docusaurus-anchor-repro.github.io',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'teikjun', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-anchor-repro', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
